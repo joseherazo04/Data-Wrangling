cbe00b780dcf8aa644833cc1d32e72c5e44fa259
José Herazo <joseherazo4@hotmail.com>
2020-04-06 15:59:37 -0500

    regex examples

--- string processing/regex-example.R
+++ string processing/regex-example.R
@@ -24,3 +24,36 @@ str_view_all(s, pattern)
 
 # this example highlight all the digits 
 str_view_all(s, "\\d")
+
+# only match ones and sevens we define the next character class: [17]
+# for all the digits it will be: [1-9] (same as \\d)
+# as in a regex everything is a string, this class represents 1,2 and 0: [1-20]
+str_view(s,"[17]")
+
+# ^ means start of string, $ means end of string
+# they are called anchors
+pattern <- "^\\d$" #start of a strinf followed by a digit followed by the end
+yes <- c("1", "5", "9")
+no <- c("12", "123", " 1", "a4", "b")
+s <- c(yes, no)
+str_view(s, pattern)
+
+# curly braces define quantifiers: 1 or 2 digits 
+pattern <- "^\\d{1,2}$" #start of string followed by one or two digit followed by the end
+yes <- c("1", "5", "9", "12")
+no <- c("123", "a4", "b")
+str_view(c(yes, no), pattern)
+
+# combining character class, anchors and quantifier
+pattern <- "^[4-7]'\\d{1,2}\"$"
+# read:
+# start of string
+# a digit between 4 and 7
+# ' character
+# one or two digits
+# " character
+# end of the string
+yes <- c("5'7\"", "6'2\"",  "5'12\"")
+no <- c("6,2\"", "6.2\"","I am 5'11\"", "3'2\"", "64")
+str_detect(yes, pattern)
+str_detect(no, pattern)
