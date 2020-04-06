404d4529c1eb040facbca822f5056fd956bfa809
José Herazo <joseherazo4@hotmail.com>
2020-04-06 16:51:12 -0500

    partial solution

--- string processing/study-case-heights.R
+++ string processing/study-case-heights.R
@@ -54,3 +54,34 @@ str_subset(problems, pattern) %>% head(n=10) %>% cat
 ind <- which(between(suppressWarnings(as.numeric(problems))/2.54, 54, 81) )
 ind <- ind[!is.na(ind)]
 problems[ind] %>% head(n=10) %>% cat
+
+#####################################################
+# Solutions
+#####################################################
+
+# function to detect entries with problems
+not_inches_or_cm <- function(x, smallest = 50, tallest = 84){
+  inches <- suppressWarnings(as.numeric(x))
+  ind <- !is.na(inches) &
+    ((inches >= smallest & inches <= tallest) |
+       (inches/2.54 >= smallest & inches/2.54 <= tallest)) #convert cm to inches
+  !ind
+}
+
+# identify entries with problems
+problems <- reported_heights %>% 
+  filter(not_inches_or_cm(height)) %>%
+  .$height
+length(problems)
+
+converted <- problems %>% 
+  str_replace("feet|foot|ft", "'") %>% #convert feet symbols to '
+  str_replace("inches|in|''|\"", "") %>%  #remove inches symbols
+  str_replace("^([4-7])\\s*[,\\.\\s+]\\s*(\\d*)$", "\\1'\\2") ##change format
+
+# find proportion of entries that fit the pattern after reformatting
+pattern <- "^[4-7]\\s*'\\s*\\d{1,2}$"
+index <- str_detect(converted, pattern)
+mean(index)
+
+converted[!index]    # show problems
