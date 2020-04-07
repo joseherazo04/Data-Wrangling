5a55c6e7454b2cde6ba1603f5fe85d350817cc1c
José Herazo <joseherazo4@hotmail.com>
2020-04-07 13:20:32 -0500

    Final solution addded

--- string processing/study-case-heights.R
+++ string processing/study-case-heights.R
@@ -124,7 +124,7 @@ s <- c("Five feet eight inches")
 str_to_lower(s)
 
 #####################################################
-# Final solution
+# Solution for the problematic entries
 #####################################################
 
 # We create a function to deal with the majority of problematic entries
@@ -163,3 +163,35 @@ index <- str_detect(remaining_problems, pattern)
 remaining_problems[!index]
 
 # There are 42 problematic entries yet, but some of them are not valid
+
+#####################################################
+# Final Solution
+#####################################################
+
+pattern <- "^([4-7])\\s*'\\s*(\\d+\\.?\\d*)$"
+
+smallest <- 50
+tallest <- 84
+new_heights <- reported_heights %>% 
+  mutate(original = height, 
+         height = words_to_numbers(height) %>% convert_format()) %>%
+  extract(height, c("feet", "inches"), regex = pattern, remove = FALSE) %>% 
+  mutate_at(c("height", "feet", "inches"), as.numeric) %>%
+  mutate(guess = 12*feet + inches) %>%
+  mutate(height = case_when(
+    !is.na(height) & between(height, smallest, tallest) ~ height, #inches 
+    !is.na(height) & between(height/2.54, smallest, tallest) ~ height/2.54, #centimeters
+    !is.na(height) & between(height*100/2.54, smallest, tallest) ~ height*100/2.54, #meters
+    !is.na(guess) & inches < 12 & between(guess, smallest, tallest) ~ guess, #feet'inches
+    TRUE ~ as.numeric(NA))) %>%
+  select(-guess)
+
+# Check entries converted
+new_heights %>%
+  filter(not_inches(original)) %>%
+  select(original, height) %>% 
+  arrange(height) %>%
+  View()
+
+
+
